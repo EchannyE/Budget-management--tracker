f28cbb0c37d84a46de54eafa571faddeab77e3d3
USER <user@MacBook-Pro.local>
2025-10-15 09:27:11 +0100

    Yinka first commit

---
+++ backend/controller/availabeFund.js
@@ -0,0 +1,23 @@
+
+
+
+const availableFund = class fund {
+    #balance;
+    constructor(amount) {
+        this.#balance = 0;
+    }
+    revenue(amount) {
+        this.#balance += amount;
+        console.log(`Total balance: ${this.#balance}`);
+    }
+    expenditure(cost) {
+        this.#balance -= cost;
+        if (this.#balance = cost) {
+            console.log(`Insufficient fund`)
+        } else {
+            console.log(`Total balance: ${this.#balance}`)
+        }
+        
+    }
+
+}
